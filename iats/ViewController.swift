cf01e1da2c74bae9a1108f92e33eb97da47b32ff
xhoiyenx <hoiyen.2000@gmail.com>
2017-01-29 06:46:44 +0700

    Initial Commit

---
+++ iats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iats
+//
+//  Created by ITConcept on 29/1/17.
+//  Copyright © 2017 IATS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
